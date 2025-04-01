e0b10aee3021d2c0da80d44a3d8e8015bedc3cc8
Archana Singh <archana21@navgurukul.org>
2025-04-01 11:41:38 +0530

    made login signup

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors';
+import mongoose from 'mongoose';
+import authRoutes from './router/authRouter.js'
+
+
+const app=express()
+app.use(cors())
+dotenv.config();
+app.use(express.json())
+
+
+const PORT=process.env.PORT
+const mongoDbUrl = process.env.mongoDb;
+
+
+mongoose.connect(mongoDbUrl,{
+    dbName: "b_net",  
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/",(req,res)=>{
+    console.log("Done")
+})
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT,function(){
+    console.log(`Server is running on Port ${PORT}`)
+})
